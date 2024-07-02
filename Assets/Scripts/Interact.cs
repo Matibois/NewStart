cf38c7c7b18f54563375953e06a2c9c2aecafc70
Matibois <100830614+Matibois@users.noreply.github.com>
2024-07-02 19:41:20 +0200

    before merge

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -6,11 +6,37 @@ using UnityEngine.UI;
 public class Interact : MonoBehaviour
 {
     [SerializeField] GameObject menu;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject parentObject;
+
+    Material[] original;
+    Material[] outline;
+    Material[] mats;
+
+    private void Start()
+    {
+        Material[] mats = parentObject.GetComponent<Renderer>().materials;
+        outline = mats;
+        original = new Material[] { outline[0], outline[0] };
+        parentObject.GetComponent<Renderer>().materials = original;
+        print("oui");
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        menu.SetActive(true);
+        if (other.CompareTag("Player"))
+        {
+            mats = outline;
+
+        }
     }
 
-    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            mats = original;
+        }
+    }
+
 }
